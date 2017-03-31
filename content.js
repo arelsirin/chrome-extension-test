8171c129f3dbddb2c31b5e2f9ff0230119fa6fa3
Arel Sirin <arels@vsisoft.com>
2017-03-31 05:05:46 -0600

    removed commented code from content.js

--- content.js
+++ content.js
@@ -1,15 +1,3 @@
-// /** 
-//  * Receive messages from background page
-//  */
-// chrome.runtime.onMessage.addListener(
-//   function(request, sender, sendResponse) {
-//     if(request.message === 'first_tab_closed') {
-//     	alert('aalala');
-//       document.body.innerHTML = 'THE TAB WAS CLOSED';
-//     }
-//   }
-// );
-
 chrome.runtime.sendMessage({message: 'tab_complete'}, function(response) {
 	chrome.storage.sync.get('activated', function(item){
 		if (!chrome.runtime.lastError && item.activated) {
